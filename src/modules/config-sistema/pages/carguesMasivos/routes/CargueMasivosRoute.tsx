4f244240720bc35648b376427c7e3998b6a54940
Desarrollador <user-48@proyelco.com>
2025-09-05 10:12:30 -0500

    Modulo de cargue masivo de update de obras

--- src/modules/config-sistema/pages/carguesMasivos/routes/CargueMasivosRoute.tsx
+++ src/modules/config-sistema/pages/carguesMasivos/routes/CargueMasivosRoute.tsx
@@ -2,12 +2,14 @@ import { RoutesWithNotFound } from "@/modules/common/components";
 import { AuthGuard } from "@/modules/common/guards";
 import { Route } from "react-router-dom";
 import { ListCargueEmpleados } from "../cargueEmpleados/pages";
+import { CargieUpdateObrasFaltantes } from "../cargieUpdateObrasFaltantes";
 
 export const CargueMasivosRoute = () => {
   return (
     <RoutesWithNotFound>
       <Route element={<AuthGuard />}>
         <Route path="/cargue-empleados" element={<ListCargueEmpleados />} />
+        <Route path="/cargue-update-proyectos" element={<CargieUpdateObrasFaltantes />} />
       </Route>
     </RoutesWithNotFound>
   );
