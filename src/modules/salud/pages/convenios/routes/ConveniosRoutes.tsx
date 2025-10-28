d1f2f3c83ac39d3dbb71d3365f797f15284762e2
Desarrollador <user-48@proyelco.com>
2025-10-28 16:08:54 -0500

    se manejan rutas que  aun no estan disponibles, se cambia link powerbi de avance

--- src/modules/salud/pages/convenios/routes/ConveniosRoutes.tsx
+++ src/modules/salud/pages/convenios/routes/ConveniosRoutes.tsx
@@ -13,8 +13,8 @@ export const ConveniosRoutes = () => {
       <Route element={<AuthGuard />}>
         <Route path="/" element={<ListConvenios />} />
         <Route path="/create" element={<FormConvenios />} />
-        <Route path="/edit/:id" element={<ModuloNoDisponilble />} />
-        <Route path="/edit-casa/:id" element={<FormConvenios />} />
+        <Route path="/edit/:id" element={<FormConvenios />} />
+        <Route path="/edit-casa/:id" element={<ModuloNoDisponilble />} />
         <Route path="/proceso/:id" element={<ResumenTorres />} />
         <Route path="/proceso/:id/detalle" element={<VistaProcesoProyectos />} />
         <Route path="/proceso-casa/:id" element={<ResumenManzanas />} />
